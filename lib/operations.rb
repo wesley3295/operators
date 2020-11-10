27c9d4496e28ab012a0664fe391408ce9f2fb677
Wesley <wspencer3295@yahoo.com>
2020-11-10 13:23:29 -0700

    commit tree ee5a80b9a37144e3595015e57544540791e243f3

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,20 @@
+
+
 def unsafe?(speed)
-
+if speed > 60
+    return true
+elsif speed < 40
+    return true
+else speed == 50
+   return false
 end
-
+end
 
 
 def not_safe?(speed)
-	
+speed > 60 ? true : false || speed < 40 ? true : false
+
+
 end
-	
 
 
